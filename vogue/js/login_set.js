2c1540060d8e99694b6810564c4308607c9b4c20
gieyoung <poohlee23@naver.com>
2024-06-05 12:08:41 +0900

    d

--- vogue/js/login_set.js
+++ vogue/js/login_set.js
@@ -81,8 +81,8 @@ ion.href = "index.php";
     );
 
     // 관리자 페이지 링크설정
-    // $(".sns a")
-    //   .last()
-    //   .click(() => (location.href = "./admin/"));
+    $(".sns a")
+      .last()
+      .click(() => (location.href = "./admin/"));
   } /////////// if /////////////////
 } ////////// loginSet 함수 /////////////////
